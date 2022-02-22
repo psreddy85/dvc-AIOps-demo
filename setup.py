9c43dd2f0cebd9d00722c0b9b717af79f0633a39
psreddy85 <sharath.reddybi@hotmail.com>
2022-02-22 21:09:19 +0000

    setup file added

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="psreddy85",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/psreddy85/dvc-AIOps-demo",
+    author_email="psreddy85@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
